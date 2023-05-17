c5754fc2a0373a04881c621ac557f001ff0397bb
nuuuwan <nuuuwan@gmail.com>
2023-05-17 09:50:59 +0530

    predict_future_arrivals - computation and chart

--- workflows/predict_future_arrivals.py
+++ workflows/predict_future_arrivals.py
@@ -1,9 +1,25 @@
+import calendar
 import os
 
+import matplotlib.pyplot as plt
 import numpy as np
-from utils import TIME_FORMAT_DATE, Time, TSVFile
+from utils import TIME_FORMAT_DATE, Time, TimeFormat, TSVFile
 
 from ml import TimeSeries
+
+
+def format_number(x: int):
+    if x > 1_000_000:
+        return f'~{x/1_000_000:.2f}M'
+
+    if x > 1_000:
+        return f'~{x/1_000:.0f}K'
+
+    return f'{x}'
+
+
+def get_month(i: int):
+    return calendar.month_abbr[i + 1]
 
 
 def parse_arrivals_data():
@@ -15,14 +31,54 @@ def parse_arrivals_data():
     return np.array(t), np.array(y)
 
 
-if __name__ == '__main__':
+def get_proj_data():
     WINDOW = 12 * 3
-    STEPS = 7
     t, y = parse_arrivals_data()
+    n_actual = int(TimeFormat('%m').stringify(Time(t[-1])))
+    n_steps = 12 - n_actual
     tseries = TimeSeries(y, window=WINDOW)
-    y_next = tseries.project(n_steps=STEPS)
+    y_next = tseries.project(n_steps=n_steps)
+    return y, y_next, n_actual
 
-    for i in range(STEPS):
-        ti = t[-1] + (i + 1) * 24 * 60 * 60 * 30
-        print(i, ti)
-        print(f'{TIME_FORMAT_DATE.stringify(Time(ti))}: {y_next[i]}')
+
+def build_monthly_chart(y, y_next, n_actual):
+    y_actual = y[-n_actual:].tolist()
+    y_with_proj = y_actual + y_next.tolist()
+    t_with_proj = [get_month(i) for i in range(12)]
+
+    fig, ax = plt.subplots()
+    fig.set_size_inches(8, 4.5)
+
+    ax.bar(t_with_proj, y_with_proj, color="lightblue", label="projection")
+    ax.bar(t_with_proj[:n_actual], y_actual, color="blue", label="actual")
+
+    ax.set_ylabel('Tourist Arrivals')
+    ax.set_title('Sri Lanka - Tourist Arrivals in 2023')
+    ax.legend()
+
+    png_file = os.path.join('charts', 'predict_future_arrivals.png')
+    plt.grid(True)
+    plt.savefig(png_file)
+    os.startfile(png_file)
+
+    total_for_year = format_number(sum(y_with_proj))
+    end_actual = t_with_proj[n_actual - 1]
+    cum_arrivals = format_number(sum(y_actual))
+    next_arrivals = format_number(y_next[0])
+    next_month = t_with_proj[n_actual]
+    print(
+        f'''
+As of the end of {end_actual} 2023,
+#SriLanka has seen {cum_arrivals} #tourist arrivals.
+
+Our model predicts {next_arrivals} arrivals in {next_month} 2023,
+and {total_for_year} total arrivals in 2023.
+
+data: https://www.sltda.gov.lk/en/statistics (@sltda_srilanka)
+    '''
+    )
+
+
+if __name__ == '__main__':
+    y, y_next, n_actual = get_proj_data()
+    build_monthly_chart(y, y_next, n_actual)
