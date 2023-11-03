5d650817e3444d22be52ec75f006341a89cbd3cc
nuuuwan <nuuuwan@gmail.com>
2023-11-03 12:59:15 +0530

    Added get_pdf_lines

--- src/tlk/common/DataReadMe.py
+++ src/tlk/common/DataReadMe.py
@@ -9,6 +9,18 @@ log = Log('DataReadMe')
 
 class DataReadMe:
     @staticmethod
+    def get_pdf_lines(pdf_link_list):
+        def get_link_line(link: Link):
+            label = link.local_file_path[15:]
+            return f'* [{label}]({label})'
+
+        inner_lines = List(pdf_link_list).map(get_link_line)
+        return [
+            '## PDFs from [SLTDA](https://www.sltda.gov.lk/statistics)',
+            f'*{len(pdf_link_list)} [PDFs](sltda/pdf) scraped*',
+        ] + inner_lines
+
+    @staticmethod
     def update(pdf_link_list: list[Link]):
         time_str = TIME_FORMAT_TIME.stringify(Time.now())
         pdf_link_list = sorted(pdf_link_list, key=lambda x: x.local_file_path)
@@ -16,11 +28,7 @@ class DataReadMe:
             '# Data (TourismLK)',
             f'*Updated {time_str}*',
             '',
-            '## PDFs from [SLTDA](https://www.sltda.gov.lk/statistics)',
-            f'*{len(pdf_link_list)} [PDFs](sltda/pdf) scraped*',
-        ] + List(pdf_link_list).map(
-            lambda link: f'* [{link.local_file_path[15:]}]({link.local_file_path[15:]})'
-        )
+        ] + DataReadMe.get_pdf_lines(pdf_link_list)
 
         file_path = os.path.join('data', 'README.md')
         File(file_path).write_lines(lines)
