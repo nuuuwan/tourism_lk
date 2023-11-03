17b277881ea29ae977c44cd0c05b46190e438c9e
nuuuwan <nuuuwan@gmail.com>
2023-11-03 13:39:11 +0530

    Added parsed link

--- src/tlk/common/DataReadMe.py
+++ src/tlk/common/DataReadMe.py
@@ -11,8 +11,9 @@ class DataReadMe:
     @staticmethod
     def get_pdf_lines(pdf_link_list):
         def get_link_line(link: Link):
-            label = link.local_file_path[15:]
-            return f'* [{label}]({label})'
+            path = link.local_file_path[15:]
+            parsed_path = path.replace('.pdf', '.pdf-parsed')
+            return f'* [{path}]({path}) ([parsed]({parsed_path}))'
 
         inner_lines = List(pdf_link_list).map(get_link_line)
         return [
