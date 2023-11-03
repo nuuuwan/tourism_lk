bd2e8439b75a06d07a105a411b6804ab31c254a5
nuuuwan <nuuuwan@gmail.com>
2023-11-03 10:09:47 +0530

    Fixed Queuing Bug

--- src/utils_future/utils_base/QueueProcessor.py
+++ src/utils_future/utils_base/QueueProcessor.py
@@ -1,5 +1,3 @@
-import queue
-
 from utils import Log
 
 from utils_future.utils_base.List import List
@@ -10,24 +8,21 @@ log = Log('QueueProcessor')
 class QueueProcessor:
     @staticmethod
     def run(initial_items, func_process, func_end):
-        q = queue.Queue()
-        for item in initial_items:
-            q.put(item)
+        q = initial_items
 
         results = []
         visited_items = set()
-        while q.not_empty and not func_end(results):
-            current_item = q.get()
+        while len(q) > 0 and not func_end(results):
+            current_item = q.pop(0)
             if current_item in visited_items:
                 continue
             visited_items.add(current_item)
-
-            log.debug(f'Visiting {item}...')
+            log.debug(f'Visiting {current_item}...')
             new_items, new_results = func_process(current_item)
-            for item in new_items:
-                q.put(item)
+            q.extend(new_items)
             results += new_results
             results = List(results).unique()
-            log.debug(f'len(results)={len(results)}')
+            log.debug(f'len(results)={len(results)}, len(q)={len(q)}.')
 
+        log.debug(f'len(results)={len(results)} final.')
         return results
